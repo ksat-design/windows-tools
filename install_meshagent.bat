add8bae6371b260117909662a5242e8fb741ea35
Shane <support@ksatdesign.com.au>
2025-06-20 12:46:45 +1000

    Update install_meshagent.bat

--- install_meshagent.bat
+++ install_meshagent.bat
@@ -1,40 +1,4 @@
 @echo off
-setlocal enabledelayedexpansion
-
-:: --- AUTO-ELEVATE IF NOT ADMIN ---
-openfiles >nul 2>&1
-if %errorlevel% neq 0 (
-    echo [INFO] Requesting administrator access...
-    powershell -Command "Start-Process '%~f0' -Verb RunAs"
-    exit /b
-)
-
-:: --- CONFIGURATION ---
-set "AGENT_URL=https://raw.githubusercontent.com/ksat-design/windows-tools/main/meshagent.exe"
-set "AGENT_DIR=%TEMP%\meshagent-install"
-set "AGENT_EXE=%AGENT_DIR%\meshagent.exe"
-
-:: --- SETUP ---
-echo [INFO] Creating install folder...
-mkdir "%AGENT_DIR%" >nul 2>&1
-cd /d "%AGENT_DIR%"
-
-echo [INFO] Downloading MeshAgent from GitHub...
-curl -L -o meshagent.exe "%AGENT_URL%"
-if not exist "%AGENT_EXE%" (
-    echo [ERROR] Download failed. Exiting.
-    pause
-    exit /b 1
-)
-
-:: --- INSTALL ---
-echo [INFO] Installing MeshAgent as a Windows service...
-"%AGENT_EXE%" -install
-
-:: --- CLEANUP ---
-echo [INFO] Cleaning up...
-cd /d %TEMP%
-rmdir /s /q "%AGENT_DIR%"
-
-echo [SUCCESS] MeshAgent is installed and running as a service.
-pause
+echo Downloading setup script...
+curl -L -o "%TEMP%\install_meshagent.ps1" https://raw.githubusercontent.com/ksat-design/windows-tools/main/install_meshagent.ps1
+powershell -ExecutionPolicy Bypass -File "%TEMP%\install_meshagent.ps1"
